c120fd441159e8b0d5b5327ed6be166e9f708ee1
Harshit <harshitkumar2503@gmail.com>
2020-08-31 09:16:36 +0530

    Rearrange_Name- Pattern Updated.

--- Rearrange_Name/rearrange_name.py
+++ Rearrange_Name/rearrange_name.py
@@ -2,7 +2,7 @@
 
 import re
 def rearrange_name(name):
-	result = re.search(r"(\w*), (\w*)$", name)
+	result = re.search(r"([\w .-]*), ([\w .-]*)$", name)
 	if result is None:
 		return name
 	return "{} {}".format(result[2], result[1])
