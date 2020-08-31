bcfc0d5673371220ac67e0a6d08263e44c68e77e
Harshit <harshitkumar2503@gmail.com>
2020-08-31 09:04:05 +0530

    Program of Rearrange_Name added.

---
+++ Rearrange_Name/rearrange_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import re
+def rearrange_name(name):
+	result = re.search(r"(\w*), (\w*)$", name)
+	if result is None:
+		return name
+	return "{} {}".format(result[2], result[1])
+
+print("Enter name in format <lastname>, <firstname>")
+name=str(input())
+res=rearrange_name(name)
+print(res)
