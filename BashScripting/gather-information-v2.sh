72fdce85697010587696d80be0d7e551f3c86769
Harshit <harshitkumar2503@gmail.com>
2020-11-02 20:03:06 +0530

    Used local variables and re-structured design

---
+++ BashScripting/gather-information-v2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+line="__________________________"
+echo "Starting at : $(date)"
+echo $line
+
+echo "PWD"
+pwd
+echo $line
+
+echo "UPTIME"
+uptime
+echo $line
+
+echo "FREE"
+free
+echo $line
+
+echo "WHO"
+who
+echo $line
+
+echo "Finishing at: $(date)"
