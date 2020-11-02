fe66406e284d947d3e859f34efc0281c9d093704
Harshit <harshitkumar2503@gmail.com>
2020-11-02 19:41:10 +0530

    Starting BashScripting

---
+++ BashScripting/gather-information.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Starting at : $(date)"
+echo
+
+echo "PWD"
+pwd
+echo
+
+echo "UPTIME"
+uptime
+echo
+
+echo "FREE"
+free
+echo
+
+echo "WHO"
+who
+echo
+
+echo "Finishing at: $(date)"
